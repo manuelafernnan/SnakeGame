aa2e783318255cb5fb9227ecaf83e0fd18877690
ManuelaF <cttmanuela@gmail.com>
2024-05-24 20:26:46 -0300

    Primeiro commit: Preparando ambiente

---
+++ Snake/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Snake
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            PnTela = new Panel();
+            LbPontos = new Label();
+            menuStrip1 = new MenuStrip();
+            Menu = new ToolStripMenuItem();
+            iniciarJogoToolStripMenuItem = new ToolStripMenuItem();
+            sairToolStripMenuItem = new ToolStripMenuItem();
+            Frame = new System.Windows.Forms.Timer(components);
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // PnTela
+            // 
+            PnTela.BorderStyle = BorderStyle.Fixed3D;
+            PnTela.Location = new Point(7, 62);
+            PnTela.Name = "PnTela";
+            PnTela.Size = new Size(428, 428);
+            PnTela.TabIndex = 0;
+            // 
+            // LbPontos
+            // 
+            LbPontos.Font = new Font("Segoe UI Semibold", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            LbPontos.Location = new Point(7, 21);
+            LbPontos.Name = "LbPontos";
+            LbPontos.Size = new Size(428, 38);
+            LbPontos.TabIndex = 1;
+            LbPontos.Text = "PONTOS: 0";
+            LbPontos.TextAlign = ContentAlignment.MiddleRight;
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { Menu });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(441, 24);
+            menuStrip1.TabIndex = 2;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // Menu
+            // 
+            Menu.DropDownItems.AddRange(new ToolStripItem[] { iniciarJogoToolStripMenuItem, sairToolStripMenuItem });
+            Menu.Name = "Menu";
+            Menu.Size = new Size(50, 20);
+            Menu.Text = "Menu";
+            // 
+            // iniciarJogoToolStripMenuItem
+            // 
+            iniciarJogoToolStripMenuItem.Name = "iniciarJogoToolStripMenuItem";
+            iniciarJogoToolStripMenuItem.Size = new Size(180, 22);
+            iniciarJogoToolStripMenuItem.Text = "Iniciar jogo";
+            // 
+            // sairToolStripMenuItem
+            // 
+            sairToolStripMenuItem.Name = "sairToolStripMenuItem";
+            sairToolStripMenuItem.Size = new Size(180, 22);
+            sairToolStripMenuItem.Text = "Sair";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(441, 498);
+            Controls.Add(LbPontos);
+            Controls.Add(PnTela);
+            Controls.Add(menuStrip1);
+            FormBorderStyle = FormBorderStyle.Fixed3D;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MainMenuStrip = menuStrip1;
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Snake II";
+            Load += Form1_Load;
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel PnTela;
+        private Label LbPontos;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem Menu;
+        private ToolStripMenuItem iniciarJogoToolStripMenuItem;
+        private ToolStripMenuItem sairToolStripMenuItem;
+        private System.Windows.Forms.Timer Frame;
+    }
+}
